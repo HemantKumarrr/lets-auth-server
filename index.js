cf2afeff58106dfc6c1a9a473fe6b2d56a4c1d1f
Hemant <hemantkumar29052001@gmail.com>
2024-12-31 00:06:01 +0530

    added dotenc

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const authRoute = require("./routes/auth.route");
